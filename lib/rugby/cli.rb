d4c9747aa32cde1e17ebbcc7130b1e3763349b07
Tim Knoll <timothy.knoll@gmail.com>
2019-02-24 04:57:41 +0000

    modified cli and building text string output

--- lib/rugby/cli.rb
+++ lib/rugby/cli.rb
@@ -9,24 +9,53 @@ class Rugby::CLI
   
   # greet user and list teams
   def greeting
-    puts "Let's play rugby!"
+    puts "Welcome rugby fan! \nLets explore one of the top rugby leagues in the world. \nSuper Rugby is comprised of 15 teams from the following counties: \nAustralia (4 teams) \nNew Zealand (5 teams) \nSouth Africa (4 teamss) \nJapan (1 team) \nArgentina (1 team)"
+    puts " "
+    puts "Below is a list of teams:"
+    puts " "
+    # list of teams method 
   end   
   
   def menu
-    
+    input = nil 
+    while input != "exit"
+      puts "What team would you like to view their players and positions? \nPlease type the team name and hit enter.  \nIf you wish to view the list of teams again, please type list and hit enter.  \nIf not interested, please type exit and hit enter."
+      input = gets.strip.downcase
+        case input
+          when "team"
+          #
+          when "list"
+          #list of teams method  
+          when "exit"
+            goodbye
+          else
+            puts "Invalid entry"
+          end
+    end         
   end   
+  
+  def submenu
+    
+  end 
+  
+  def goodbye
+    puts " "
+    puts "Thank you!  Have a nice day and come back soon."
+    puts " "
+  end   
+  
 end   
 
 
-cli 
+# cli 
 
-greeting
+# greeting
 
-choose three conferences
-list teams based on conference picked
-list includes team name and url 
-pick team
-pull team roster - player name and player position
+# choose three conferences
+# list teams based on conference picked
+# list includes team name and url 
+# pick team
+# pull team roster - player name and player position
 
 
   
