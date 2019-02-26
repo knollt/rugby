fbdd7bed33600983424703b1e48360463e3ec283
Tim Knoll <timothy.knoll@gmail.com>
2019-02-26 23:24:34 +0000

    url and team scrape WIP

--- lib/rugby/scrape.rb
+++ lib/rugby/scrape.rb
@@ -9,13 +9,17 @@ class Rugby::Scrape
     # puts "in scrape"
     webpage = Nokogiri::HTML(open("https://www.rugby.com.au/competitions/super-rugby"))
     section = webpage.css("div.col-xs-12.col-md-4.col-xl-2")
-    binding.pry
-    array_of_teams = section.css("div.team__name.meta-team__name span.link-text")
+    # binding.pry
     
-    array_of_teams.each do |team|
-      binding.pry
-      Rugby::Team.new(team.text)
+    # section.css("a.module.team.pullout-xs.pullout--padded")
+    # section.css("a.module.team.pullout-xs.pullout--padded div.team__name.meta-team__name span.link-text")[0].text
+    # section.css("a.module.team.pullout-xs.pullout--padded div.team__name.meta-team__name span.link-text")[1].text
+    array_of_url = section.css("a.module.team.pullout-xs.pullout--padded div.team__name.meta-team__name span.link-text")
+    
+    array_of_url.each do |url|
       # binding.pry
+      Rugby::Team.new(team.text)
+      binding.pry
     end   
   end
 end  
