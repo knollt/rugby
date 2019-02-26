cb9bfecbdf9c2cb47751981a1644ba55262d8c17
Tim Knoll <timothy.knoll@gmail.com>
2019-02-26 23:02:28 +0000

    scraped team name

--- lib/rugby/scrape.rb
+++ lib/rugby/scrape.rb
@@ -9,10 +9,12 @@ class Rugby::Scrape
     # puts "in scrape"
     webpage = Nokogiri::HTML(open("https://www.rugby.com.au/competitions/super-rugby"))
     section = webpage.css("div.col-xs-12.col-md-4.col-xl-2")
+    binding.pry
     array_of_teams = section.css("div.team__name.meta-team__name span.link-text")
     
     array_of_teams.each do |team|
-      team.text
+      binding.pry
+      Rugby::Team.new(team.text)
       # binding.pry
     end   
   end
