ce1c723198e4baa231ae9d8913751ac7037cdeea
Tim Knoll <timothy.knoll@gmail.com>
2019-02-26 02:30:18 +0000

    modified team.rb

--- lib/rugby/team.rb
+++ lib/rugby/team.rb
@@ -1,3 +1,20 @@
 class Rugby::Team 
+  # create reader/writer methods/attributes or properties for each climbing city 
+  attr_accessor :name, :url 
+  
+  @@all = []
+  
+  #initialize a team instance with properties and adds to @@all array 
+  def initialize (name, url)
+    @name = name
+    @url = url
+    @@all << self
+  end 
+  
+  #call all team objects
+  def self.all
+    @@all
+  end 
+  
   
 end   
