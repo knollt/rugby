0687c46888d71e06cec91b95f8652ecde7af4a0a
Tim Knoll <timothy.knoll@gmail.com>
2019-02-27 04:24:15 +0000

    url_team scrape still WIP

--- lib/rugby/scrape.rb
+++ lib/rugby/scrape.rb
@@ -7,26 +7,31 @@ class Rugby::Scrape
   
   def self.scrape_teams
     # puts "in scrape"
-    webpage = Nokogiri::HTML(open("https://www.rugby.com.au/competitions/super-rugby"))
-    section = webpage.css("div.col-xs-12.col-md-4.col-xl-2")
+    doc = Nokogiri::HTML(open("https://www.rugby.com.au/competitions/super-rugby"))
+    section = doc.css("div.col-xs-12.col-md-4.col-xl-2")
     # binding.pry
     
-    array_of_url = section.css("a.module.team.pullout-xs.pullout--padded")
-    # section.css("a.module.team.pullout-xs.pullout--padded div.team__name.meta-team__name span.link-text")[0].text
-    # section.css("a.module.team.pullout-xs.pullout--padded div.team__name.meta-team__name span.link-text")[1].text
-    array_of_teams = section.css("a.module.team.pullout-xs.pullout--padded div.team__name.meta-team__name span.link-text")
+
     
-    array_of_url.each do |url|
+    section.each do |team|
       # binding.pry
-      Rugby::Team.new(url.attributes['href'].value)
-      # binding.pry
-    end
-    
-    array_of_teams.each do |team|
-      # binding.pry
-      Rugby::Team.new(team.text)
-      # binding.pry
+      url = team.css("a.module.team.pullout-xs.pullout--padded.attributes['href'].value")
+      team = team.css("a.module.team.pullout-xs.pullout--padded div.team__name.meta-team__name span.link-text").text
+      binding.pry
     end
     
   end
 end  
+
+
+
+    # array_of_url = section.css("a.module.team.pullout-xs.pullout--padded")
+    # section.css("a.module.team.pullout-xs.pullout--padded div.team__name.meta-team__name span.link-text")[0].text
+    # section.css("a.module.team.pullout-xs.pullout--padded div.team__name.meta-team__name span.link-text")[1].text
+    # array_of_teams = section.css("a.module.team.pullout-xs.pullout--padded div.team__name.meta-team__name span.link-text")
+    
+    # array_of_url.each do |url|
+    #   # binding.pry
+    #   Rugby::Team.new(url.attributes['href'].value)
+    #   # binding.pry
+    # end
